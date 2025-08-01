c9d3f94a7a750ab6efe93d05ff6d76a5fb3512f0
caiocroccia22 <caiocroccia22@gmail.com>
2025-07-31 22:00:12 -0300

    feat: implment prevent error file

--- main.go
+++ main.go
@@ -8,10 +8,11 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Biblioteca godot env -> para visualizar as váriavies do arquivo .env
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	fmt.Println(os.Getenv("Teste"))
+	fmt.Println(os.Getenv("PORT"))
 }
---
+++ src/configuration/rest_err/rest_err.go
@@ -0,0 +1,83 @@
+// Arquivo de padronização de erros -> Para todos os erros da aplicação estarem em um lugar
+// Evitar usar o nome err para não dar conflito com o pacote nativo do go
+package resterr_test
+
+import "net/http"
+
+// Criando um objeto do tipo struct
+type RestErr struct {
+	// Campo mensagem vai ter a mensagem do erro
+	//json -> Deixar o nosso objeto exportável para JSON
+	Message string `json: "message"`
+	// Campo da stack trace do que aconteceu
+	Err string `json: "error"`
+	// Campo code vai guardar qual o código da requisição que estamos dando para o cliente
+	Code int64 `json: "code"`
+	// Campo Cause do tipo Causes vai guardar quais as causas de erro da aplicação
+	Causes []Causes `json: "causes"`
+}
+
+//Vai ser um array de um objeto
+type Causes struct {
+	// Dentro desse objeto vão ter os campos Field e Message
+	Field   string `json: "field"`
+	Message string `json: "message"`
+}
+
+// Criando um constructor para o objeto
+// *RestErr -> Significa retorno
+func NewRestErr(message, err string, code int64, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     err,
+		Code:    code,
+		Causes:  causes,
+	}
+}
+
+// Função implementada apatir de RestErro-> Para satisfazer a interface de error do go
+func (r *RestErr) Error() string {
+	return r.Message
+}
+
+// Função de badRequest
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+	}
+}
+
+func NewBadRequestErrorValidationError(message string, causes []Causes) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "bad_request",
+		Code:    http.StatusBadRequest,
+		Causes:  causes,
+	}
+}
+
+func InternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Internal_server_error",
+		Code:    http.StatusInternalServerError,
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Not found",
+		Code:    http.StatusNotFound,
+	}
+}
+
+func NewForbiddenError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Err:     "Forbidden",
+		Code:    http.StatusForbidden,
+	}
+}
