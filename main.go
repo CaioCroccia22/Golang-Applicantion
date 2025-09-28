779675a2d22edcb5d13e5c9392373968756c95a2
caiocroccia22 <caiocroccia22@gmail.com>
2025-09-28 19:43:03 -0300

    feat: implement docker connection

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"crud_application/src/configuration/database/mongodb"
 	"crud_application/src/configuration/logger"
+	"crud_application/src/controller"
 	"crud_application/src/controller/routes"
+	"crud_application/src/model/service"
 	"log"
 
 	"github.com/gin-gonic/gin"
@@ -16,6 +19,16 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	// Chamando o pacote de conexão com o banco
+	mongodb.InitConnection()
+
+	// Inicialização de dependências
+	// Com a criação do arquivo user_controller.go será necessário criar a instância para uma estrutura unica do service
+	// essa estrutura sera chamada no controller
+	service := service.NewuserDomainService()
+	userController := controller.NewUserControllerInterface(service)
+
 	// Inicialização de rotas
 	router := gin.Default() //gin.Default inicializa o roteador com logger e middlewares de recovery
 	/*
@@ -28,7 +41,7 @@ func main() {
 	// Criação de logs
 	logger.Logger.Info("About to start user application") //>> {"level":"info","time":"2025-08-24T14:36:48.321-0300","message":"About to start user application"}
 
-	routes.InitRoute(&router.RouterGroup)
+	routes.InitRoute(&router.RouterGroup, userController)
 	if err := router.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
